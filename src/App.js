14a17093675f88ce76e6c927c26eb5d08add7e8e
Jonas <jgalmeida1993@gmail.com>
2020-05-29 13:29:08 -0300

    :art: Remove wrong import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { SWRConfig } from "swr";
 import "rbx/index.css";
-import { Grid } from "react-styled-flexboxgrid";
+
 import Crimes from "./components/Crimes";
 
 export default function App() {
